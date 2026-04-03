6490af7d99e87378ddc375031a06cfcc2cfec491
crl1005 <carlosesguerra001@gmail.com>
2026-04-03 20:31:16 +0800

    pls

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,15 @@
 import type { Config } from 'tailwindcss'
 
-export default {
+const config: Config = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-} satisfies Config
+}
+
+export default config
