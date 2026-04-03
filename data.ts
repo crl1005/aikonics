8c8624ee9fc4df4e01c250352e0c8de02c600127
crl1005 <carlosesguerra001@gmail.com>
2026-04-03 18:11:27 +0800

    fix

---
+++ data.ts
@@ -0,0 +1,11 @@
+import { Video } from "@/types";
+
+export const VIDEOS: Video[] = [
+  {
+    id: "1",
+    title: "Sample Video",
+    url: "https://your-video-url.com",
+    thumbnail: "",
+    description: "Sample description"
+  }
+]
