36aead48d30aa6f479b2c158d87b44f13b98dfb7
crl1005 <carlosesguerra001@gmail.com>
2026-04-03 21:18:02 +0800

    kapagod

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,13 +4,13 @@ interface LogoProps {
 
 export default function Logo({ onClick }: LogoProps) {
   return (
-    <a
+    
       href="#"
       onClick={e => { e.preventDefault(); onClick?.(); }}
       className="flex flex-col no-underline text-slate-100"
     >
-      <span className="text-2xl tracking-wide font-serif">Aikonics</span>
-      <span className="text-[10px] tracking-[0.2em] uppercase text-slate-400">Journey</span>
+      <span className="text-3xl tracking-wide font-serif">Aikonics</span>
+      <span className="text-[11px] tracking-[0.25em] uppercase text-slate-400">Journey</span>
     </a>
   );
 }
