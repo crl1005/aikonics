5f375bd0865e58b918b206b6b156f1527daba9dd
crl1005 <carlosesguerra001@gmail.com>
2026-04-03 21:20:31 +0800

    add

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,13 +4,9 @@ interface LogoProps {
 
 export default function Logo({ onClick }: LogoProps) {
   return (
-    
-      href="#"
-      onClick={e => { e.preventDefault(); onClick?.(); }}
-      className="flex flex-col no-underline text-slate-100"
-    >
+    <a href="#" onClick={e => { e.preventDefault(); onClick?.(); }} className="flex flex-col no-underline text-slate-100">
       <span className="text-3xl tracking-wide font-serif">Aikonics</span>
-      <span className="text-[11px] tracking-[0.25em] uppercase text-slate-400">Journey</span>
+      <span className="text-[11px] tracking-[0.25em] uppercase text-slate.400">Journey</span>
     </a>
   );
 }
